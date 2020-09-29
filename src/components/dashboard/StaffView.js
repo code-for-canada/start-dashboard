cb331c7d0909c8788f1fa1515032367e8968d5ec
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-09-28 20:34:10 -0400

    Construct panels and sidebar from same data source.

--- src/components/dashboard/StaffView.js
+++ src/components/dashboard/StaffView.js
@@ -42,7 +42,7 @@ const MoveDownButton = props => (
 )
 
 const ViewControlBlockItem = props => {
-  const { name, isVisible, firstItem, lastItem } = props
+  const { title, isVisible, firstItem, lastItem } = props
 
   const moveDown = () => {
     console.log('moving down!')
@@ -54,9 +54,9 @@ const ViewControlBlockItem = props => {
 
   return(
     <tr>
-      <td className="align-middle"><nobr>{name}</nobr></td>
+      <td className="align-middle"><nobr>{title}</nobr></td>
       <td>
-        <div className="btn-group" role="group" aria-label={`Actions on view: ${name}`}>
+        <div className="btn-group" role="group" aria-label={`Actions on view: ${title}`}>
             <ShowHideButton isVisible={isVisible} />
             <MoveUpButton disabled={firstItem} onClick={moveUp} />
             <MoveDownButton disabled={lastItem} onClick={moveDown} />
@@ -66,46 +66,25 @@ const ViewControlBlockItem = props => {
   )
 }
 
-const ViewControlBlock = () => {
-  const initialData = [
-    {
-      name: 'Project Updates',
-      isVisible: true,
-      order: 0,
-    },
-    {
-      name: 'Submit Update',
-      isVisible: true,
-      order: 1,
-    },
-    {
-      name: 'Artwork Status Board',
-      isVisible: false,
-      order: 2,
-    },
-    {
-      name: 'Submissions',
-      isVisible: true,
-      order: 3,
-    },
-    {
-      name: 'Artworks',
-      isVisible: true,
-      order: 4,
-    },
-    {
-      name: 'Artists',
-      isVisible: false,
-      order: 5,
-    },
-  ]
+const ViewControlBlock = props => {
+  const { data } = props
+  const sortByOrder = (a, b) => {
+    return a.order - b.order
+  }
 
   return(
     <>
       <h4 className="my-3">All views</h4>
       <table className="table table-sm">
         <tbody>
-          {initialData.map((view, index) => <ViewControlBlockItem {...view} key={view.name} firstItem={index === 0} lastItem={index === initialData.length - 1} />)}
+          {data.sort(sortByOrder).map((view, index) =>
+            <ViewControlBlockItem
+              {...view}
+              key={view.title}
+              firstItem={index === 0}
+              lastItem={index === data.length - 1}
+            />
+          )}
         </tbody>
       </table>
     </>
@@ -121,6 +100,63 @@ const Sidebar = props => (
 )
 
 const StaffDashboard = ({user}) => {
+  const panelsData = [
+    {
+      title: 'Project Updates',
+      isVisible: true,
+      isSmall: true,
+      order: 0,
+      editLink: 'https://airtable.com/tblhqR67jrTJ169Cf/viwvQN6OyFyxsPYtq?blocks=hide',
+      editText: 'Edit in Airtable',
+      frameSrc: 'https://airtable.com/embed/shrxrrqGr1tu5UKt9?backgroundColor=red&viewControls=on',
+    },
+    {
+      title: 'Submit Update',
+      isVisible: true,
+      isSmall: true,
+      order: 1,
+      editLink: 'https://airtable.com/tblhqR67jrTJ169Cf/viwV5AQuGxE4OfNX0?blocks=hide',
+      editText: 'Edit in Airtable',
+      frameSrc: 'https://airtable.com/embed/shr087J79r2jG6rE2?backgroundColor=red',
+    },
+    {
+      title: 'Artwork Status Board',
+      isVisible: false,
+      isSmall: false,
+      order: 2,
+      editLink: 'https://airtable.com/tbl5ApSEOzPpe4fwp/viwiX18oxXONzk8th?blocks=hide',
+      editText: 'Edit in Airtable',
+      frameSrc: 'https://airtable.com/embed/shrTlL5928ssPbMeP?backgroundColor=red&viewControls=on',
+    },
+    {
+      title: 'Submissions',
+      isVisible: true,
+      isSmall: false,
+      order: 3,
+      editLink: 'https://streetartto.submittable.com/submissions',
+      editText: 'Edit in Submittable',
+      frameSrc: 'https://airtable.com/embed/shrqukWs4K0JgixB9?backgroundColor=red&viewControls=on',
+    },
+    {
+      title: 'Artworks',
+      isVisible: true,
+      isSmall: false,
+      order: 4,
+      editLink: 'https://airtable.com/tbl5ApSEOzPpe4fwp/viwfmyIqZl3bsj2eo?blocks=hide',
+      editText: 'Edit in Airtable',
+      frameSrc: 'https://airtable.com/embed/shrdDGqIxvtiIjFzZ?backgroundColor=red&viewControls=on',
+    },
+    {
+      title: 'Artists',
+      isVisible: false,
+      isSmall: false,
+      order: 5,
+      editLink: 'https://www.cognitoforms.com/forms/artistprofile/entries',
+      editText: 'Edit in CognitoForms',
+      frameSrc: 'https://airtable.com/embed/shrJegAZi7w7Kj5ue?backgroundColor=red&viewControls=on',
+    },
+  ]
+
   return (
     <Container>
       <div className="row">
@@ -135,34 +171,24 @@ const StaffDashboard = ({user}) => {
             <div className="col">
               <Container>
                 <Row>
-                  <Panel title='Progress Updates' defaultOpen={true} defaultSmall={true} editLink="https://airtable.com/tblhqR67jrTJ169Cf/viwvQN6OyFyxsPYtq?blocks=hide" editText="Edit in Airtable">
-                    <EmbeddedIframe title="Artwork Progress Updates" src="https://airtable.com/embed/shrxrrqGr1tu5UKt9?backgroundColor=red&viewControls=on" />
-                  </Panel>
-
-                  <Panel title='Submit Update' defaultOpen={true} defaultSmall={true} editLink="https://airtable.com/tblhqR67jrTJ169Cf/viwV5AQuGxE4OfNX0?blocks=hide" editText="Edit in Airtable">
-                    <EmbeddedIframe title="Artwork Progress Update Form" src="https://airtable.com/embed/shr087J79r2jG6rE2?backgroundColor=red" />
-                  </Panel>
-
-                  <Panel title='Artwork Status Board' defaultOpen={true} defaultSmall={false} editLink="https://airtable.com/tbl5ApSEOzPpe4fwp/viwiX18oxXONzk8th?blocks=hide" editText="Edit in Airtable">
-                    <EmbeddedIframe title="Artwork Status Board" src="https://airtable.com/embed/shrTlL5928ssPbMeP?backgroundColor=red&viewControls=on" />
-                  </Panel>
-
-                  <Panel title='Submissions' defaultOpen={true} defaultSmall={false} editLink="https://streetartto.submittable.com/submissions" editText="Edit in Submittable">
-                    <EmbeddedIframe title="Submissions" src="https://airtable.com/embed/shrqukWs4K0JgixB9?backgroundColor=red&viewControls=on" />
-                  </Panel>
-
-                  <Panel title='Artworks' defaultOpen={true} defaultSmall={false} editLink="https://airtable.com/tbl5ApSEOzPpe4fwp/viwfmyIqZl3bsj2eo?blocks=hide" editText="Edit in Airtable">
-                    <EmbeddedIframe title="Artworks" src="https://airtable.com/embed/shrdDGqIxvtiIjFzZ?backgroundColor=red&viewControls=on" />
-                  </Panel>
-
-                  <Panel title='Artists' defaultOpen={true} defaultSmall={false} editLink="https://www.cognitoforms.com/forms/artistprofile/entries" editText="Edit in CognitoForms">
-                    <EmbeddedIframe title="Artists" src="https://airtable.com/embed/shrJegAZi7w7Kj5ue?backgroundColor=red&viewControls=on" />
-                  </Panel>
+                  {panelsData.map((panel) =>
+                    <Panel
+                      {...panel}
+                      key={panel.title}
+                      defaultOpen={panel.isVisible}
+                      defaultSmall={panel.isSmall}
+                    >
+                      <EmbeddedIframe
+                        title={panel.title}
+                        src={panel.frameSrc}
+                      />
+                    </Panel>
+                  )}
                 </Row>
               </Container>
             </div>
             <Sidebar>
-              <ViewControlBlock />
+              <ViewControlBlock data={panelsData} />
             </Sidebar>
           </div>
         </Container>
