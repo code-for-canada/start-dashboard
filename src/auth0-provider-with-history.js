fdf39dbce02832797569ea29b3976be925fed056
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-10-19 23:27:36 -0400

    eslint: Small fix.

--- src/auth0-provider-with-history.js
+++ src/auth0-provider-with-history.js
@@ -24,7 +24,7 @@ const Auth0ProviderWithHistory = ({ children }) => {
   )
 }
 Auth0ProviderWithHistory.propTypes = {
-  children: PropTypes.element
+  children: PropTypes.node
 }
 
 export default Auth0ProviderWithHistory
