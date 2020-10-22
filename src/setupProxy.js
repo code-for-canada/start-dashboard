c1710f29cc3aaceb36a9e628a5e0624699496304
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-10-22 10:31:35 -0400

    eslint: Fixes.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,12 +3,12 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 // This file overrides CRA's simple proxying, which doesn't
 // easily allow proxied /api endpoints to be viewable in browser.
 // See: https://stackoverflow.com/a/40897087/504018
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
       target: 'http://localhost:8080',
-      changeOrigin: true,
+      changeOrigin: true
     })
   )
 }
