825583eba126244a39cf33f10bc9c10ae4a15750
Sharon Kennedy <sharon@nomadiclabs.ca>
2020-10-28 21:51:59 -0400

    open profile endpoint for cognito support

--- src/views/profile.js
+++ src/views/profile.js
@@ -15,99 +15,25 @@ const Profile = () => {
   const location = useLocation()
   const history = useHistory()
 
-  // fetch the artist profile for the authed user
-  useEffect(() => {
-    const abortController = new AbortController()
-    const getArtist = async () => {
-      try {
-        const res = await fetch(
-          `/api/artist?email=${encodeURIComponent(user.email)}`,
-          { signal: abortController.signal }
-        )
-        const data = await res.json()
-        if (data.records.length > 0) {
-          const artistRecord = data.records[0]
-          setArtist({ ...artistRecord.fields, id: artistRecord.id })
-        }
-        setLoading(false)
-      } catch (err) {
-        if (abortController.signal.aborted) {
-          console.log('Request to fetch Submittable forms was aborted')
-        } else {
-          console.log('Error fetching Submittable forms', err)
-          setLoading(false)
-        }
-      }
-    }
-
-    getArtist()
-
-    return () => {
-      abortController.abort()
-    }
-  }, [user])
-
-  // checks on whether to show profile or not
-  useEffect(() => {
-    const hash = location.hash
-    const hasProfile = !!hash
-
-    // authed user with no profile can see empty form
-    if (!hasProfile && user) {
-      setShowProfile(true)
-    }
-
-    // StART Staff can see all profiles
-    if (
-      hasProfile &&
-      user['https://streetartoronto.ca/role'] === 'StART Staff'
-    ) {
-      setShowProfile(true)
-    }
-
-    // artist can only see their own profile
-    if (hasProfile && artist) {
-      const isOwnProfile =
-        artist.edit_url.includes(hash) || artist.view_url.includes(hash)
-      setShowProfile(isOwnProfile)
-    }
-  }, [artist, user, location])
-
-  if (isLoading) {
-    return <Loading />
-  }
-
-  if (showProfile) {
-    return (
-      <DefaultLayout>
-        <EmbeddedCognitoForm
-          formId={COGNITO_FORMS_IDS.artistProfile}
-          opts={{
-            entry: {
-              PersonalInformation: {
-                EmailAddress: user.email
-              }
+  return (
+    <DefaultLayout>
+      <EmbeddedCognitoForm
+        formId="12"
+        opts={{
+          entry: {
+            PersonalInformation: {
+              EmailAddress: user.email
             }
-          }}
-          afterSubmit={(event, entry) => {
-            event.preventDefault()
-            history.push('/dashboard')
-          }}
-        />
-      </DefaultLayout>
-    )
-  }
+          }
+        }}
+        afterSubmit={(event, entry) => {
+          event.preventDefault()
+          history.push('/dashboard')
+        }}
+      />
+    </DefaultLayout>
+  )
 
-  // not their profile
-  if (!showProfile) {
-    return (
-      <DefaultLayout>
-        <Container>
-          <p className="mt-4 mb-1">You must be logged in to see this page.</p>
-        </Container>
-      </DefaultLayout>
-    )
-  }
 }
 
 export default Profile
