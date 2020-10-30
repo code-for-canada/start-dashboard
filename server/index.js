b69c3c2ae061a76fecdf4b0ad8e8ce652dd3063a
Sharon Kennedy <sharon@nomadiclabs.ca>
2020-10-30 14:08:30 -0400

    trying new server setup for heroku

--- server/index.js
+++ server/index.js
@@ -1,34 +1,53 @@
+require('dotenv').config()
+
 const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
-require('dotenv').config()
 
-const handleLocations = require('./api/location')
-const handleArtist = require('./api/artist')
-const handleForms = require('./api/forms')
+const isDev = process.env.NODE_ENV !== 'production';
+const PORT = process.env.PORT || 8080;
 
-const app = express();
+// Multi-process to utilize all CPU cores.
+if (!isDev && cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.all('/api/location', handleLocations)
-app.all('/api/artist', handleArtist)
-app.all('/api/forms', handleForms)
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-// Only in production is the server the main entry point,
-// so only then serve built static files from filesystem.
-//
-// (In development, React files are served by webpack-dev-server
-// from memory, which also proxies the API. See: `src/setupProxy.js` )
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, '..', 'build')));
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+  });
 
-  // Handle React routing, return all other requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+} else {
+  const app = express();
+
+  const handleLocations = require('./api/location')
+  const handleArtist = require('./api/artist')
+  const handleForms = require('./api/forms')
+
+  // Priority serve any static files.
+  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+
+  // Answer API requests.
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.all('/api/location', handleLocations)
+  app.all('/api/artist', handleArtist)
+  app.all('/api/forms', handleForms)
+
+
+  // All remaining requests return the React app, so it can handle routing.
+  app.get('*', function(request, response) {
+    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+  });
+
+  app.listen(PORT, function () {
+    console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
   });
 }
 
-app.listen(process.env.PORT || 3000);
