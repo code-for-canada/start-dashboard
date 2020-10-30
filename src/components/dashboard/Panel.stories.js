33d1542299570caa87d32192fa0e07584fa3790b
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-10-30 18:36:02 -0400

    Added CustomIframe story to Panel component.

--- src/components/dashboard/Panel.stories.js
+++ src/components/dashboard/Panel.stories.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Panel from './Panel'
 import { action } from '@storybook/addon-actions'
+import EmbeddedIframe from './EmbeddedIframe'
 
 export default {
   component: Panel,
@@ -8,13 +9,12 @@ export default {
 }
 
 const Template = args => (
-  <Panel {...args}>
-    <div style={{height: '100%', backgroundColor: 'lightgray'}} />
-  </Panel>
+  <Panel {...args} />
 )
 
 export const Default = Template.bind({})
 Default.args = {
+  children: <div style={{height: '100%', backgroundColor: 'lightgray'}} />,
   isVisible: false,
   isSmall: true,
   title: "Example Panel",
@@ -44,4 +44,14 @@ export const Small = Template.bind({})
 Small.args = {
   ...Default.args,
   isSmall: true
+}
+
+export const CustomIframe = Template.bind({})
+CustomIframe.args = {
+  ...Default.args,
+  isVisible: true,
+  title: 'Some Panel',
+  editText: 'Edit on Example.com',
+  editLink: 'https://example.com',
+  children: <EmbeddedIframe src="https://example.com" />
 }
