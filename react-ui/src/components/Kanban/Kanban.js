6a5744ab1ba35d5205b09fc38ebc8ea90a9af773
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-11-04 04:13:13 -0500

    Added artworks kanban data. Added Dialog component to Kanban.

--- react-ui/src/components/Kanban/Kanban.js
+++ react-ui/src/components/Kanban/Kanban.js
@@ -1,26 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import Board from 'react-trello'
 import CustomCard from './CustomCard'
+import { Dialog, DialogTitle, DialogActions, DialogContent, DialogContentText } from '@material-ui/core'
+import Slide from '@material-ui/core/Slide'
+import { Button } from 'react-bootstrap'
 
-const Kanban = ({ data = {}, onCardClick }) => {
+const DialogTransition = React.forwardRef(function DialogTransition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />
+})
+
+const Kanban = ({ data = {}, onCardClick, onDataChange, onDragEnd }) => {
+  const [ showCard, setShowCard ] = useState(false)
+  const [ activeCard, setActiveCard ] = useState({})
+
+  const handleCardClick = (cardId, metadata, laneId) => {
+    onCardClick(cardId, metadata, laneId)
+    setActiveCard(metadata || {})
+    setShowCard(true)
+  }
+
+  const handleClose = () => {
+    setShowCard(false)
+  }
+
   return (
-    <Board
-      {...{onCardClick}}
-      data={data}
-      draggable
-      laneDraggable={false}
-      hideCardDeleteIcon
-      components={{
-        Card: CustomCard
-      }}
-    />
+    <>
+      <Board
+        onCardClick={handleCardClick}
+        onDataChange={onDataChange}
+        handleDragEnd={onDragEnd}
+        data={data}
+        draggable
+        laneDraggable={false}
+        hideCardDeleteIcon
+        components={{
+          Card: CustomCard
+        }}
+      />
+      <Dialog
+        onClose={handleClose}
+        open={showCard}
+        TransitionComponent={DialogTransition}
+      >
+        <DialogTitle>{activeCard.title}</DialogTitle>
+        <DialogContent dividers>
+          <DialogContentText>
+            <pre>
+              {JSON.stringify(activeCard, null, 2)}
+            </pre>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">Close</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   )
 }
 
 Kanban.propTypes = {
   data: PropTypes.object,
-  onCardClick: PropTypes.func
+  onCardClick: PropTypes.func,
+  onDataChange: PropTypes.func,
+  onDragEnd: PropTypes.func
 }
 
 export default Kanban
--- react-ui/src/components/Kanban/Kanban.stories.js
+++ react-ui/src/components/Kanban/Kanban.stories.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import Kanban from './Kanban'
 
-const data = require('./../../../.storybook/data/kanban.json')
+const data = require('./../../../.storybook/data/kanban-artworks.json')
+
+const populateMetadata = data => ({
+  lanes: data.lanes.map(lane => ({
+    ...lane,
+    label: lane.cards.length.toString(),
+    cards: lane.cards.map(card => ({
+      ...card,
+      metadata: card
+    }))
+  }))
+})
+
+const populatedData = populateMetadata(data)
 
 export default {
   component: Kanban,
@@ -12,5 +25,5 @@ const Template = args => <Kanban {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
-  data: data
+  data: populatedData
 }
