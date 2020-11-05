937c1a33312daed6ffd51560db7eac89a531b074
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-11-05 00:21:42 -0500

    Added fully fleshed out CustomCard component and story.

--- react-ui/src/components/Kanban/CustomCard.js
+++ react-ui/src/components/Kanban/CustomCard.js
@@ -1,8 +1,89 @@
-import React from 'react'
-import Card from 'react-trello/dist/components/Card'
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 
-const CustomCard = props => (
-  <Card {...props} />
-)
+import {
+  MovableCardWrapper,
+  CardHeader,
+  CardRightContent,
+  CardTitle,
+  Detail,
+  Footer
+} from 'react-trello/dist/styles/Base'
+import Tag from 'react-trello/dist/components/Card/Tag'
+import DeleteButton from 'react-trello/dist/widgets/DeleteButton'
+
+class CustomCard extends Component {
+  onDelete = e => {
+    this.props.onDelete()
+    e.stopPropagation()
+  }
+
+  render()  {
+    const {
+      showDeleteButton,
+      style,
+      tagStyle,
+      onClick,
+      //onDelete,
+      className,
+      id,
+      title,
+      label,
+      description,
+      tags,
+      cardDraggable
+    } = this.props
+
+    return (
+      <MovableCardWrapper
+        data-id={id}
+        onClick={onClick}
+        style={style}
+        className={className}
+      >
+        <CardHeader>
+          <CardTitle draggable={cardDraggable}>{title}</CardTitle>
+          <CardRightContent>{label}</CardRightContent>
+          {showDeleteButton && <DeleteButton onClick={this.onDelete} />}
+        </CardHeader>
+        <Detail>{description}</Detail>
+        {tags && tags.length> 0 && (
+          <Footer>
+            {tags.map(tag => (
+              <Tag key={tag.title} {...tag} tagStyle={tagStyle} />
+            ))}
+          </Footer>
+        )}
+      </MovableCardWrapper>
+      )
+  }
+}
+
+CustomCard.propTypes = {
+  showDeleteButton: PropTypes.bool,
+  onDelete: PropTypes.func,
+  onClick: PropTypes.func,
+  style: PropTypes.object,
+  tagStyle: PropTypes.object,
+  className: PropTypes.string,
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  description: PropTypes.string,
+  tags: PropTypes.array,
+}
+
+CustomCard.defaultProps = {
+  showDeleteButton: true,
+  onDelete: () => {},
+  onClick: () => {},
+  style: {},
+  tagStyle: {},
+  title: 'no title',
+  description: '',
+  label: '',
+  tags: [],
+  className: ''
+}
 
 export default CustomCard
