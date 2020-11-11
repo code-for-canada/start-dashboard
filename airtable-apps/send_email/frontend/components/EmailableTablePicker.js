6952426ffee971dae977ce804932656a18491a6e
Sharon Kennedy <sharon@nomadiclabs.ca>
2020-11-11 08:30:45 -0500

    nitpicking

--- airtable-apps/send_email/frontend/components/EmailableTablePicker.js
+++ airtable-apps/send_email/frontend/components/EmailableTablePicker.js
@@ -2,20 +2,17 @@ import React, { useEffect, useState } from 'react'
 import { Select, useBase } from '@airtable/blocks/ui'
 import { isTableEmailable } from './../utils'
 
-const EmailableTablePicker = ({ useBaseHook = useBase, ...props}) => {
+const EmailableTablePicker = ({ useBaseHook = useBase, ...props }) => {
   const base = useBaseHook()
   const [ filteredOptions, setFilteredOptions ] = useState([])
 
   useEffect(() => {
-    const loadEmailableOptions = () => {
-      const emailableOptions = base.tables.map(table => ({
-        value: table.id,
-        label: table.name,
-        disabled: !isTableEmailable(table)
-      }))
-      setFilteredOptions(emailableOptions)
-    }
-    loadEmailableOptions()
+    const emailableOptions = base.tables.map(table => ({
+      value: table.id,
+      label: table.name,
+      disabled: !isTableEmailable(table)
+    }))
+    setFilteredOptions(emailableOptions)
   }, [setFilteredOptions, base])
 
   return (
