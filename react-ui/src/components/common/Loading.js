3d3eea6cd7af87ab5f1bc9cccdec1d7ee8f04cb9
Sharon Kennedy <sharon@nomadiclabs.ca>
2021-01-23 11:26:53 -0500

    fix spinner size

--- react-ui/src/components/common/Loading.js
+++ react-ui/src/components/common/Loading.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 import loading from 'assets/images/loading.svg'
 
-const Loading = () => (
-  <div className="spinner">
-    <img src={loading} alt="Loading" />
-  </div>
-)
+const useStyles = makeStyles({
+  spinner: {
+    position: 'absolute',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'rgba(255, 255, 255, 0.75)',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 2,
+  },
+})
+
+const Loading = () => {
+  const classes = useStyles()
+  return (
+    <div className={classes.spinner}>
+      <img src={loading} alt="Loading" />
+    </div>
+  )
+}
 
 export default Loading
