5e6dc3305cceaf638bf93e6a1e888d6e3be9af71
Sharon Kennedy <sharon@nomadiclabs.ca>
2021-02-18 15:17:05 -0500

    only load cognito script on pages with embedded forms adn wait for script to load before initializing form

--- react-ui/src/components/forms/EmbeddedCognitoForm.js
+++ react-ui/src/components/forms/EmbeddedCognitoForm.js
@@ -3,35 +3,51 @@ import PropTypes from 'prop-types'
 
 const EmbeddedCognitoForm = ({ formId, showForm, opts, afterSubmit }) => {
   const [cognitoLoaded, setCognitoLoaded] = useState(false)
+  const [scriptLoaded, setScriptLoaded] = useState(false)
   const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const script = document.createElement('script')
+
+    script.src = 'https://www.cognitoforms.com/s/vQtvojkwk0qKXX6uRXdPYA'
+    script.async = true
+    script.onload = () => setScriptLoaded(true)
+
+    document.body.appendChild(script)
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, [])
+
+  const loadCognitoForm = () => {
+    try {
+      window.Cognito.load(
+        'forms',
+        {
+          id: formId,
+          ...opts
+        },
+        {
+          success: () => {
+            window.ExoJQuery(() => {
+              window.ExoJQuery(document).on('afterSubmit.cognito', afterSubmit)
+            })
+          }
+        }
+      )
+      setCognitoLoaded(true)
+    } catch (err) {
+      console.log(err)
+      setError('There was an error loading the form. Please try again.')
+    }
+  }
 
   // load the embedded CognitoForm
   useEffect(() => {
-    if (!cognitoLoaded && showForm) {
-      try {
-        window.Cognito.load(
-          'forms',
-          {
-            id: formId,
-            ...opts
-          },
-          {
-            success: () => {
-              window.ExoJQuery(() => {
-                window
-                  .ExoJQuery(document)
-                  .on('afterSubmit.cognito', afterSubmit)
-              })
-            }
-          }
-        )
-        setCognitoLoaded(true)
-      } catch (err) {
-        console.log(err)
-        setError('This form is not available.')
-      }
+    if (scriptLoaded && !cognitoLoaded && showForm) {
+      loadCognitoForm()
     }
-  }, [showForm, cognitoLoaded, formId, opts, afterSubmit])
+  }, [showForm, cognitoLoaded, formId, opts, afterSubmit, scriptLoaded])
 
   if (error || !showForm) {
     const errorMessage = error || 'This form is not available'
